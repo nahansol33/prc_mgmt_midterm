17dd658f981a0866c0559de21ee5d7a52630e581
nahansol33 <134092157+nahansol33@users.noreply.github.com>
2025-03-04 16:30:30 -0500

    made a new change by Hansol Na

--- src/main/java/nahans/App.java
+++ src/main/java/nahans/App.java
@@ -4,10 +4,12 @@ package nahans;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
+
+// add comment
+
+// changes made in the app.java file by Hansol Na -- added comment
